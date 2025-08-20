d46d981dc1456fb32001abb2803672c7fb864fa3
GaluhHungkul <muhamadgaluh31@gmail.com>
2025-08-20 18:46:01 +0700

    ui/add bg color to WhyRentWithUs components

--- src/components/LandingPage/WhyRentWithUs.tsx
+++ src/components/LandingPage/WhyRentWithUs.tsx
@@ -23,13 +23,13 @@ const WhyRentWithUs = () => {
     transition={{
       duration : .4
     }}
-    className='mt-20 lg:pb-10 md:mt-28 lg:mt-38 scroll-mt-6'>
+    className='mt-20 py-10 md:py-16 md:mt-28 lg:mt-38 scroll-mt-6 bg-tertiary w-screen relative text-primary right-10 lg:right-20 px-10 lg:px-20'>
         <h1 className='text-center font-bold text-2xl md:text-3xl'><span className='text-secondary'>Why Rent</span> with Us?</h1>
         <ul className='grid md:grid-cols-2 mt-10 gap-y-6 gap-x-4 lg:grid-cols-4'>
            {WhyRentWithUsPoints.map(({content, icon},i) => (
-            <li key={i} className='flex flex-col items-center gap-4 bg-secondary/20 rounded px-6 py-4 hover:bg-secondary/40 duration-300 md:space-y-4 lg:py-8 lg:px-6 group hover:-translate-y-6 shadow-xl hover:shadow-2xl '>
-                <span className='md:scale-125 text-secondary'>{icon}</span>
-                <p className='text-center text-xs font-semibold md:text-base'>{content}</p>
+            <li key={i} className='flex flex-col items-center gap-4 bg-secondary rounded px-6 py-4 hover:bg-secondary duration-300 md:space-y-4 lg:py-8 lg:px-6 group hover:-translate-y-6 shadow-xl hover:shadow-2xl '>
+                <span className='md:scale-125 text-primary'>{icon}</span>
+                <p className='text-center text-xs font-semibold md:text-base text-tertiary'>{content}</p>
             </li>
            ))}
         </ul>
